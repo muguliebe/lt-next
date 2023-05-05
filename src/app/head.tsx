20978f0790167ee46d8bbbff84ee68911bc799f0
zany <muguliebe@gmail.com>
2023-05-05 23:07:20 +0900

    init

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>lt-next</title>
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='zany' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  )
+}
